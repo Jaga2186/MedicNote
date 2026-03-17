c4c2d22f0dde1260b91be4dd281df4edb855edd4
Jaga2186 <jagadhesh.85131@gmail.com>
2026-03-17 23:51:12 +0530

    add validation constraints (Min, Max) to Doctor entity fields

--- doctorService/src/main/java/com/MedicNote/doctorService/entity/Doctor.java
+++ doctorService/src/main/java/com/MedicNote/doctorService/entity/Doctor.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
