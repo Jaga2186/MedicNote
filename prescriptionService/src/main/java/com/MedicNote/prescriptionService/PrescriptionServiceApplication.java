1af0b0af8baf0510ceca1fe427de1fc703d6d561
Jaga2186 <jagadhesh.85131@gmail.com>
2026-03-23 14:52:35 +0530

    refactor: rename application classes for consistency

---
+++ prescriptionService/src/main/java/com/MedicNote/prescriptionService/PrescriptionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.MedicNote.prescriptionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PrescriptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescriptionServiceApplication.class, args);
+	}
+
+}
