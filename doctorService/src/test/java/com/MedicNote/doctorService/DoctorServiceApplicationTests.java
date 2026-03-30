0413bbfb834c078dcbdad73b6bcce3aa28f07ffd
Jaga2186 <jagadhesh.85131@gmail.com>
2026-03-30 21:51:52 +0530

    Add DoctorService test classes

---
+++ doctorService/src/test/java/com/MedicNote/doctorService/DoctorServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.MedicNote.doctorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DoctorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
