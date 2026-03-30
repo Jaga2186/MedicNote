99e90762d5426a7fa9b97afe8c4f3991384c2917
Jaga2186 <jagadhesh.85131@gmail.com>
2026-03-30 21:50:54 +0530

    Update test classes and resources for PatientService and PrescriptionService

--- prescriptionService/src/test/java/com/MedicNote/prescriptionService/ApplicationTests.java
+++ prescriptionService/src/test/java/com/MedicNote/prescriptionService/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.NedicNote.prescriptionService;
+package com.MedicNote.prescriptionService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
