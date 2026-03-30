94632f8df22463530b911e8e62e4cae6a4c1dad6
Jaga2186 <jagadhesh.85131@gmail.com>
2026-03-30 21:48:18 +0530

    Add ErrorCode enum for DoctorService exceptions

---
+++ doctorService/src/main/java/com/MedicNote/doctorService/exception/ErrorCode.java
@@ -0,0 +1,11 @@
+package com.MedicNote.doctorService.exception;
+
+public enum ErrorCode {
+
+    DOCTOR_NOT_FOUND,
+    DOCTOR_ALREADY_EXISTS,
+    INVALID_CREDENTIALS,
+    BAD_REQUEST,
+    VALIDATION_FAILED,
+    INTERNAL_SERVER_ERROR
+}
